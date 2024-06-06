80fb781ed4f2afb720c2f4f3781371030019eb99
alieddine <ali.eddiine@gmail.com>
2024-06-06 22:34:43 +0100

    idk

--- src/FetchEvents.jsx
+++ src/FetchEvents.jsx
@@ -2,41 +2,32 @@ import React, { useState, useRef } from 'react';
 import axios from "axios";
 
 const FetchEvents = () => {
-  const [events, setEvents] = useState([{"a":""}]);
+  const [events, setEvents] = useState({"response":"", "done":""});
   const [dataInput, setDataInput] = useState("");
   const [dataHist, setDataHist] = useState("");
   const [loading, setLoading] = useState(false);
-  const eventsRef = useRef([]);
+ 
 
-  console.log(dataInput)
+ 
   const fetchEvents = async () => {
     setLoading(true);
-    const url = 'http://127.0.0.1:8000/stream'; // Replace with your actual URL
-
+    const url = 'http://127.0.0.1:8000/test2';  
     try {
       const data = {"input":dataInput, "hist":dataHist}
       const response = await fetch(url, { method: 'POST'  ,  headers: {'Content-Type':'application/json'},body: JSON.stringify(data)});
-      // const response = await axios.post(url, {"input":dataInput});
-
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-
       const reader = response.body.getReader();
       const decoder = new TextDecoder('utf-8');
-
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
-
         const chunk = decoder.decode(value, { stream: true });
-        const lines = chunk.split('\n').filter(Boolean);
-
-        lines.forEach(line => {
-          const event = JSON.parse(line);
-          eventsRef.current = [...eventsRef.current, event];
-          setEvents(eventsRef.current);
-        });
+        console.log(chunk)
+        setEvents(JSON.parse(chunk));
+        console.log("parse")
+       
       }
     } catch (error) {
       console.error('Fetch error:', error);
@@ -61,7 +52,12 @@ const FetchEvents = () => {
      
  
     </div>
-     <div className="right">{events[events.length -1]["a"]}</div>
+     <div className="right">
+      <div className="">{events["response"]}</div>
+ 
+      <div className="done">{ events["done"].toString()}</div>
+      </div>
+    
      </div>
   );
 };
